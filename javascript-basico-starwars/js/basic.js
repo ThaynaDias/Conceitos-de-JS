e94d0d734518e08b349823f61ce20fdd70098059
THAYNA NASCIMENTO <thaynadiasnascimento.com>
2023-11-08 21:19:32 -0300

    ajuste de comentario

--- javascript-basico-starwars/js/basic.js
+++ javascript-basico-starwars/js/basic.js
@@ -18,12 +18,12 @@ var jedi = true
 var n1 = 5
 var n2 = 6.7
 
-//console.log(typeof n1)
-//console.log(typeof n2)
+console.log(typeof n1)
+console.log(typeof n2)
 
-//var total = n1 / n2 
+var total = n1 / n2 
 
-//console.log(total)
+console.log(total)
 
 
 
